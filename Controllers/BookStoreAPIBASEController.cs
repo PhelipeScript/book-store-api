ffe15718702e3e78b13b32e5454fe4e9bc448387
Phelipe Pereira <phelipescript@gmail.com>
2025-05-03 23:20:55 -0300

    Create the base controller

---
+++ Controllers/BookStoreAPIBASEController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookStoreAPI.Controllers;
+[Route("[controller]")]
+[ApiController]
+public abstract class BookStoreAPIBASEController : ControllerBase
+{
+
+}
