e87e1522bd878754629905679c7abb3680f7575b
Phelipe Pereira <phelipescript@gmail.com>
2025-05-04 00:43:12 -0300

    Create a singleton from the IBook Repository interface to be able to use the Book Repository with dependency injection

--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
+using BookStoreAPI.Repositories;
+using BookStoreAPI.Repositories.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddSingleton<IBookRespository, BookRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
