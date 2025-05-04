25489fc9ac69db3a2ab47b9d05561dca8e72ae19
Phelipe Pereira <phelipescript@gmail.com>
2025-05-04 00:38:12 -0300

    Create Book Controller with a GET endpoint that fetches books

---
+++ Controllers/BookController.cs
@@ -0,0 +1,24 @@
+﻿using BookStoreAPI.Entities;
+using BookStoreAPI.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStoreAPI.Controllers;
+
+public class BookController : BookStoreAPIBASEController
+{
+    private readonly IBookRespository _bookRespository;
+
+    public BookController(IBookRespository bookRespository)
+    {
+        _bookRespository = bookRespository;
+    }
+
+    [HttpGet("fetch")]
+    [ProducesResponseType(typeof(List<Book>), StatusCodes.Status200OK)]
+    public IActionResult Fetch()
+    {
+        var books = _bookRespository.Fetch();
+
+        return Ok(books);
+    }
+}
