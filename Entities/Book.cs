643967be9e74cd1eeeca0c24bd5bf82d9c928e35
Phelipe Pereira <phelipescript@gmail.com>
2025-05-04 00:31:36 -0300

    Create Book Entity

---
+++ Entities/Book.cs
@@ -0,0 +1,12 @@
+﻿namespace BookStoreAPI.Entities;
+
+public class Book
+{
+    public string Id { get; set; } = string.Empty;
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string Author { get; set; } = string.Empty;
+    public string Genre { get; set; } = string.Empty;
+    public float Value { get; set; } = 0.0f;
+    public int Quantity { get; set; } = 0;
+}
