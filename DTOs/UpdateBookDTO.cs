1b23c9dc6651e0d6f58f2035c7105a75fd147d40
Phelipe Pereira <phelipescript@gmail.com>
2025-05-07 00:40:18 -0300

    Create UpdateBookDTO class

---
+++ DTOs/UpdateBookDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace BookStoreAPI.DTOs;
+
+public class UpdateBookDTO
+{
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string Author { get; set; } = string.Empty;
+    public string Genre { get; set; } = string.Empty;
+    public float Value { get; set; } = 0.0f;
+    public int Quantity { get; set; } = 0;
+}
